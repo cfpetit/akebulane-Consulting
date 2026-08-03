394353ea8d289142b3ca79693c6f92c00b6068d5
cfpetit <carlosfrancia@usal.es>
2026-08-03 19:36:50 +0000

    Fixed config

--- config/prod.py
+++ config/prod.py
@@ -1,12 +1,16 @@
 # config/prod.py
 
+import os
 from .default import *
 
 
-SECRET_KEY = '5e04a4955d8878191923e86fe6a0dfb24edb226c87d6c7787f35ba4698afc86e95cae409aebd47f7'
+SECRET_KEY = os.getenv('SECRET_KEY', '5e04a4955d8878191923e86fe6a0dfb24edb226c87d6c7787f35ba4698afc86e95cae409aebd47f7'
 
 APP_ENV = APP_ENV_PRODUCTION
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://akebulan_prod:secure_password@localhost/akebulane_prod'
+SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or os.getenv('DATABASE_URL')
+
+if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
 
 ADMINS = ['cfranciapetit@gmail.com']
